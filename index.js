140a00544c39265d9c79e4ddee1955fb92845b58
Daniel Segura <danielseguraf@gmail.com>
2022-02-19 23:18:49 -0500

    creating help command

--- index.js
+++ index.js
@@ -1,12 +1,55 @@
 #!/usr/bin/env node
-import chalk from 'chalk';
+import meow from 'meow';
 import boxen from 'boxen';
 import init from './utils/init.js';
+import chalk from 'chalk';
 
-init();
+import {
+  newLine,
+  textSpace,
+  mainTitle,
+  contactTitle,
+  location,
+  email,
+  linkedIn,
+  github,
+  skillsTitle,
+  skillsLine1,
+  skillsLine2,
+  skillsLine3,
+  aboutMeTitle,
+  meText1,
+  meText2,
+  meText3,
+} from './utils/data.js';
+
+const helpText = `
+  Usage
+    npx daniel-resume [options]
+
+  Options
+    contact         Show contact info
+    --no-contact    Don't show contact info
+    skills          Show skills info
+    --no-skills     Don't show skills info
+`;
+
+const options = {
+  importMeta: import.meta,
+  flags: {
+    contact: { type: 'boolean', default: true },
+    skills: { type: 'boolean', default: true },
+  },
+};
+
+const cli = meow(helpText, options);
 
 (function () {
+  init();
+
   const log = console.log;
+
+  log(`cli`, cli.flags);
 
   // Styles
   const headline = chalk.white.bold;
@@ -22,57 +65,28 @@ init();
   const link = chalk.yellow.underline;
   const bold = chalk.bold;
 
-  // Content
-  const newLine = '\n';
-  const textSpace = '\b';
-
-  const mainTitle = `Daniel Segura Fang - Frontend Developer 🚀`;
-
-  const contactTitle = 'Contact Information';
-  const location = 'Lima, Peru (UTC-5)';
-  const email = 'hello@danielsegura.dev';
-  const linkedIn = 'https://www.linkedin.com/in/daniel-segura-fang/';
-  const github = 'https://github.com/daniseguraf';
-
-  const skillsTitle = 'Tech Skills';
-  const skillsLine1 = chalk.white(`React - Typescript - JavaScript - Node.js`);
-  const skillsLine2 = chalk.white(
-    `Express - Storybook - Styled-components - Jest`
-  );
-  const skillsLine3 = chalk.white(`React Testing Library - HTML - CSS - SCSS`);
-
-  const aboutMeTitle = 'About Me';
-
-  const react = bold('React.js');
-  const typescript = bold('TypeScript');
-  const javascript = bold('JavaScript');
-  const node = bold('Node.js');
-  const next = bold('Next.js');
-
-  const meText1 = `Front-end developer with +5 years of experience with a keen eye for design and high UI/UX criteria. I have built highly scalable, modular, maintainable end-to-end solutions using ${react}, ${typescript}, ${javascript}, ${node}, ${next}, among others. My goal is to contribute to building world-class software solutions with highly performant dynamic teams and, at the same time, support and contribute with my experience in the growth of the product, the team, and the company.`;
-
-  const meText2 = `If you believe that I might fit with your interests, please feel free to contact me in LinkedIn or write me to ${email},`;
-
-  const meText3 = `Thanks 🙌!`;
-
   // prints
   const headlineModule = boxen(headline(mainTitle), headlineBox);
   log(headlineModule);
 
-  log(heading(contactTitle));
-  log(text(location));
-  log(text(email));
-  log(link(linkedIn));
-  log(link(github));
+  if (cli.flags.contact) {
+    log(heading(contactTitle));
+    log(text(location));
+    log(text(email));
+    log(link(linkedIn));
+    log(link(github));
 
-  log(newLine);
+    log(newLine);
+  }
 
-  log(heading(skillsTitle));
-  log(text(skillsLine1));
-  log(text(skillsLine2));
-  log(text(skillsLine3));
+  if (cli.flags.skills) {
+    log(heading(skillsTitle));
+    log(text(skillsLine1));
+    log(text(skillsLine2));
+    log(text(skillsLine3));
 
-  log(newLine);
+    log(newLine);
+  }
 
   log(heading(aboutMeTitle));
   log(text(meText1));
---
+++ utils/data.js
@@ -0,0 +1,42 @@
+// Content
+const newLine = '\n';
+const textSpace = '\b';
+
+const mainTitle = `Daniel Segura Fang - Frontend Developer 🚀`;
+
+const contactTitle = 'Contact Information';
+const location = 'Lima, Peru (UTC-5)';
+const email = 'hello@danielsegura.dev';
+const linkedIn = 'https://www.linkedin.com/in/daniel-segura-fang/';
+const github = 'https://github.com/daniseguraf';
+
+const skillsTitle = 'Tech Skills';
+const skillsLine1 = `React - Typescript - JavaScript - Node.js`;
+const skillsLine2 = `Express - Storybook - Styled-components - Jest`;
+const skillsLine3 = `React Testing Library - HTML - CSS - SCSS`;
+const aboutMeTitle = 'About Me';
+
+const meText1 = `Front-end developer with +5 years of experience with a keen eye for design and high UI/UX criteria. I have built highly scalable, modular, maintainable end-to-end solutions using React, TypeScript, JavaScript, Node, among others. My goal is to contribute to building world-class software solutions with highly performant dynamic teams and, at the same time, support and contribute with my experience in the growth of the product, the team, and the company.`;
+
+const meText2 = `If you believe that I might fit with your interests, please feel free to contact me in LinkedIn or write me to ${email},`;
+
+const meText3 = `Thanks 🙌!`;
+
+export {
+  newLine,
+  textSpace,
+  mainTitle,
+  contactTitle,
+  location,
+  email,
+  linkedIn,
+  github,
+  skillsTitle,
+  skillsLine1,
+  skillsLine2,
+  skillsLine3,
+  aboutMeTitle,
+  meText1,
+  meText2,
+  meText3,
+};
